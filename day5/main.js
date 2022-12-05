6a6c4478419f4b0922e5d357dcc99cbe2b429e10
LG <luca.galeno@gmail.com>
2022-12-05 11:36:05 +0100

    no messages

--- day5/main.js
+++ day5/main.js
@@ -1,30 +1,41 @@
 // day5
 
 const fs = require('fs');
-const fileName = 'test.txt'
+const fileName = 'input.txt'
+
 
 const data = fs.readFileSync(fileName, {encoding:'utf8'}).split('\n')
 
-const dictionaryOfStack = {
-    1: ['F','L','M','W'],
-    2: ['F','M','V','Z','B'],
-    3: ['Q','L','S','R','V','H'],
-    4: ['J','T','M','P','Q','V','S','F'],
-    5: ['W','S','L'],
-    6: ['W','J','R','M','P','V','F'],
-    7: ['F','R','N','P','C','Q','J'],
-    8: ['B','R','W','Z','S','P','H','V'],
-    9: ['W','Z','H','G','C','J','M','B']
-}
 
-const dictionaryTest = {
-    1: ['N','Z'],
-    2: ['D','C','M'],
+const dictionaryOfStack = {
+    1: ['W','M','L','F'],                 
+    2: ['B','Z','V','M','F'],               
+    3: ['H','V','R','S','L','Q'],           
+    4: ['F','S','V','Q','P','M','T','J'],  
+    5: ['L','S','W'],                       
+    6: ['F','V','P','M','R','J','W'],      
+    7: ['J','Q','C','P','N','R','F'],      
+    8: ['V','H','P','S','Z','W','R','B'],  
+    9: ['B','M','J','C','G','H','Z','W']    
+}
+/* //test:
+const data = ['move 1 from 2 to 1',
+                'move 3 from 1 to 3',
+                'move 2 from 2 to 1',
+                'move 1 from 1 to 2']
+
+
+const dictionaryOfStack = {
+    1: ['Z','N'],
+    2: ['M','C','D'],
     3: ['P']
 }
+*/
 
-//test move 1 from 2 to 1
 const procedure = data => {
+
+    let finalString = '' 
+    const dictionaryLength = Object.keys(dictionaryOfStack).length
 
     data.forEach(element => {
         element = element.replace(/[\r\n]/g, '')
@@ -33,21 +44,38 @@ const procedure = data => {
         const fromStack = +(/from (\d+)/g).exec(element)[1]
         const toStack = +(/to (\d+)/g).exec(element)[1]
 
+        const arrayFrom = Object.values(dictionaryOfStack[fromStack])
+        const arrayTo = Object.values(dictionaryOfStack[toStack])
+
         for (i=0;i<movedNumber;i++){
-                
-            const arrayfrom = Object.values(dictionaryTest[fromStack])
-            const arrayto = Object.values(dictionaryTest[toStack])
+            //part 1
+            //arrayto.push(arrayfrom[arrayfrom.length-1])
+            //arrayfrom.pop()
 
-            arrayto.push(arrayfrom[arrayfrom.length-1])
-            arrayfrom.pop()
-
-            dictionaryTest[fromStack] = arrayfrom;
-            dictionaryTest[toStack] = arrayto                    
-
+            //dictionaryOfStack[fromStack] = arrayfrom;
+            //dictionaryOfStack[toStack] = arrayto                    
         }
 
+        // part 2
+        const sliceArray = (arrayFrom.slice(-movedNumber))
+        arrayFrom.splice(arrayFrom.length - movedNumber, movedNumber)
+            
+        sliceArray.forEach(element => {
+            arrayTo.push(element)
+        })
+            
+        dictionaryOfStack[fromStack] = arrayFrom;
+        dictionaryOfStack[toStack] = arrayTo        
+
     })
-    console.log(dictionaryTest)
+
+    //read last element of values array in the object
+    for(j=1;j<=dictionaryLength;j++) {
+        arrayDictionary = Object.values(dictionaryOfStack[j])
+        finalString += arrayDictionary[arrayDictionary.length-1] 
+    }
+    return finalString
+
 }
 
-procedure(data)
+console.log(procedure(data))
